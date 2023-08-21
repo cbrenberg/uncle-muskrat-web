3df87f799ee0a4dc05b1642730821160b3b83d1d
Christopher Brenberg <cbrenberg@gmail.com>
2023-08-20 19:50:04 -0500

    remove unused scroll fixing code

--- js/custom.js
+++ js/custom.js
@@ -5,9 +5,6 @@
   jQuery(window).load(function () {
     jQuery("#preloader").delay(100).fadeOut("slow");
     jQuery("#load").delay(100).fadeOut("slow");
-
-    // fixes issue with variable viewport height on mobile
-    // $(function(){ $('body').css('height',$(window).height()); })
   });
 
 })(jQuery);
