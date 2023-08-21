a2f3807c340422f4e975191665e1b50bc216dda2
Christopher Brenberg <cbrenberg@gmail.com>
2023-08-20 19:12:21 -0500

    javascript fix for mobile scrolling bug

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,9 @@
   jQuery(window).load(function () {
     jQuery("#preloader").delay(100).fadeOut("slow");
     jQuery("#load").delay(100).fadeOut("slow");
+
+    // fixes issue with variable viewport height on mobile
+    $('body').css('height',$(window).height());
   });
 
 })(jQuery);
