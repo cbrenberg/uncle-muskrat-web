ac9d24a2c58897ddc64a9bdf08a1e9f40d83db58
Christopher Brenberg <cbrenberg@gmail.com>
2025-05-13 16:55:15 -0500

    Revert "display summer tour poster as modal on page load (#12)" (#13)
    This reverts commit f3c259f759818eee6dc5ac5c730270f71ab89cd8.

--- js/custom.js
+++ js/custom.js
@@ -3,27 +3,8 @@
   new WOW().init();
 
   jQuery(window).load(function () {
-    jQuery("#preloader").delay(25).fadeOut("slow");
-    jQuery("#load").delay(25).fadeOut("slow");
-    // Get the modal
-    var modal = document.getElementById("myModal");
-    // When the page loads, open the modal 
-    modal.style.display = "block";
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("modal-close")[0];
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      //if (event.target == modal) {
-        modal.style.display = "none";
-      //}
-    }
+    jQuery("#preloader").delay(100).fadeOut("slow");
+    jQuery("#load").delay(100).fadeOut("slow");
   });
 
 })(jQuery);
